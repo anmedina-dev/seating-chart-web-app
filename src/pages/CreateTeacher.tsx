ac437a518d4822e7f6661a7fda5f15a1f5218f50
anmedina-dev <antranikmedina@gmail.com>
2023-04-25 13:33:30 -0400

    fix linting issues

--- src/pages/CreateTeacher.tsx
+++ src/pages/CreateTeacher.tsx
@@ -103,8 +103,8 @@ export default function CreateTeacher() {
       </header>
 
       <h1 className="mb-10">
-        Looks like you're new! Add some info about yourself before creating your
-        classroom!
+        Looks like you&apos;re new! Add some info about yourself before creating
+        your classroom!
       </h1>
       <form className="w-full max-w-lg">
         <div className="flex flex-wrap -mx-3 mb-6">
