230703269c6b9dae343ee2e36dd66c332dad94f5
anmedina-dev <antranikmedina@gmail.com>
2023-06-21 17:16:24 -0400

    Update the routing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
 import styles from "../styles/Home.module.css";
-//import { useRouter } from "next/router";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/router";
 
 export default function Home() {
   const { isLoaded, isSignedIn, user } = useUser();
-  // const router = useRouter();
+  const router = useRouter();
 
-  //if (isLoaded && isSignedIn) redirect("/CreateTeacher");
+  if (isLoaded && isSignedIn) router.push("/CreateTeacher");
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
